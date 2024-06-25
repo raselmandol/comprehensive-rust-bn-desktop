f598046a8a253bb390c8acb99dfc61227a0313df
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2024-06-25 23:52:42 +0600

    Update main.js

--- base/main.js
+++ base/main.js
@@ -4,14 +4,15 @@ let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
-    width: 1920,
-    height: 1080,
-    webPreferences: {
+    width: 1920, //init width
+    height: 1080, //init height
+    webPreferences: { //set pref--> for Node.js APIs inst
       nodeIntegration: true, //Allows the use of Node.js APIs in this web app
     },
   });
 
-  mainWindow.loadFile('index.html'); //For loading the main HTML file
+  //set the entry point
+  mainWindow.loadFile('index.html'); //For loading the main HTML file 
 
   mainWindow.on('closed', () => {
     mainWindow = null;
